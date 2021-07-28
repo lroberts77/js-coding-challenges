6348f49a8b5fd62640f95bac3a0d1e7a733cb525
Liam R <liamroberts77@googlemail.com>
2021-07-28 16:32:45 +0100

    updated methods of an array challenge

--- JS-challenges/methods-of-arrayObject-map/methods-of-arrayObject-map.js
+++ JS-challenges/methods-of-arrayObject-map/methods-of-arrayObject-map.js
@@ -1,8 +1,10 @@
-var a = ["abcd","efjgh"]
-    var b = a.map((x) => {
+isolateIt = (arr) => {
+    var b = arr.map((x) => {
       return (x.length % 2 == 0 ? 
         x.slice(0,x.length/2) + "|" + x.slice(x.length/2)
         : 
         x.slice(0,x.length/2) + "|" + x.slice(x.length/2 + 1))
     })
-    console.log(b);
+    return b
+  }
+  console.log(isolateIt(["abcd","efjgh"]));
