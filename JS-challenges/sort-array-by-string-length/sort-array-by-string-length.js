64501c224ca6f2d09d81df5a98170198c9b12724
Liam R <liamroberts77@googlemail.com>
2021-08-15 14:44:34 +0100

    completed challenge

---
+++ JS-challenges/sort-array-by-string-length/sort-array-by-string-length.js
@@ -0,0 +1,6 @@
+sortByLength = (array) => {
+    return array.sort((a,b) => a.length - b.length)
+  };
+console.log(sortByLength(["Beg", "Life", "I", "To"]));  
+console.log(sortByLength(["", "Moderately", "Brains", "Pizza"]));
+console.log(sortByLength(["Longer", "Longest", "Short"]));
