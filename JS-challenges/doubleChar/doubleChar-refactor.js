7fed2305a3a615fae40aaf93dadd2766ee4a66c8
Liam R <liamroberts77@googlemail.com>
2021-08-15 15:40:41 +0100

    refactored completed challenge

---
+++ JS-challenges/doubleChar/doubleChar-refactor.js
@@ -0,0 +1,14 @@
+doubleChar = (str) => {
+    return str.split('').map((a) => a+a).join('')
+}
+console.log(doubleChar('hello'))
+console.log(doubleChar("Adidas"));
+
+//regex 
+
+// doubleChar = (str) => {
+//     return str.replace(/(.)/g, "$1$1")
+//   }
+// console.log(doubleChar('hello'))
+// console.log(doubleChar("Adidas"));
+  
