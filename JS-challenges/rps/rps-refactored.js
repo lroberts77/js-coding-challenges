689da39e23566db4d4879395a9c99ba17134aa7c
Liam R <liamroberts77@googlemail.com>
2021-08-18 22:36:34 +0100

    refactored rps challenge

---
+++ JS-challenges/rps/rps-refactored.js
@@ -0,0 +1,11 @@
+const rps = (p1, p2) => {
+    return p1 == 'rock' && p2 == 'paper'||
+    p1 == 'scissors' && p2 =='rock' || 
+    p1 == 'paper' && p2 == 'scissors' ? 'Player 2 won!' : 
+    p1 === p2 ? 'Draw!' : 
+    'Player 1 won!'
+  };
+  console.log(rps('rock', 'scissors'));
+  console.log(rps('paper', 'rock'));
+  console.log(rps('scissors', 'rock'));
+  console.log(rps('rock', 'rock'));
--- JS-challenges/rps/rps.js
+++ JS-challenges/rps/rps.js
@@ -1,11 +1,18 @@
 const rps = (p1, p2) => {
+    // return p1 == 'rock' && p2 == 'paper'
+    // || p1 == 'scissors' && p2 =='rock' 
+    // || p1 == 'paper' && p2 == 'scissors' ? 'Player 2 won!' : 
+    // p1 == 'paper' && p2 == 'rock' 
+    // || p1 == 'rock' && p2 =='scissors' 
+    // || p1 == 'scissors' && p2 == 'paper' ? 'Player 1 won!' : 'Draw!'
+
     return p1 == 'rock' && p2 == 'paper'
     || p1 == 'scissors' && p2 =='rock' 
     || p1 == 'paper' && p2 == 'scissors' ? 'Player 2 won!' : 
-    p1 == 'paper' && p2 == 'rock' 
-    || p1 == 'rock' && p2 =='scissors' 
-    || p1 == 'scissors' && p2 == 'paper' ? 'Player 1 won!' : 'Draw!'
+    p1 === p2 ? 'Draw!' : 
+    'Player 1 won!'
   };
   console.log(rps('rock', 'scissors'));
+  console.log(rps('paper', 'rock'));
   console.log(rps('scissors', 'rock'));
   console.log(rps('rock', 'rock'));
