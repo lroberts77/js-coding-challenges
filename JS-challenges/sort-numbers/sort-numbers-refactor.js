d4fca4608462045bdf6372ecbb28b8cf1db17193
Liam R <liamroberts77@googlemail.com>
2021-08-20 11:40:43 +0100

    refactored passed challenge

---
+++ JS-challenges/sort-numbers/sort-numbers-refactor.js
@@ -0,0 +1,7 @@
+solution = (nums) => {
+    return !nums ? [] : nums.sort((a,b) => a-b)
+  }
+console.log(solution([1,2,3,10,5]));
+console.log(solution(null));
+console.log(solution([]));
+console.log(solution([20, 2, 10]));
