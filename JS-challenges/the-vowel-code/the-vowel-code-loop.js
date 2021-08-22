4892ca22f20da66570fede5417c51d79fd40a200
Liam R <liamroberts77@googlemail.com>
2021-08-22 23:46:13 +0100

    completed fourth approach

--- JS-challenges/the-vowel-code/the-vowel-code-loop.js
+++ JS-challenges/the-vowel-code/the-vowel-code-loop.js
@@ -1,3 +1,19 @@
+var vowels = [...'aeiou']
+encode = (string) => {
+  var s = "";
+  for (var i = 0; i < string.length; i++) {
+    s += vowels.indexOf(string[i])+1 || string[i];
+  }
+  return s;
+}
+
+decode = (string) => {
+  var s = "";
+  for (var i = 0; i < string.length; i++) {
+    s += vowels[string[i]-1] || string[i];
+  }
+  return s;
+}
 
 
 console.log(encode('hello'));
