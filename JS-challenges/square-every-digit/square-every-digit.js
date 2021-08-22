cead6ab2a762ce060e11afe9ca1233664737a8f3
Liam R <liamroberts77@googlemail.com>
2021-08-22 16:13:48 +0100

    added new challenge and task

--- JS-challenges/square-every-digit/square-every-digit.js
+++ JS-challenges/square-every-digit/square-every-digit.js
@@ -1,5 +1,6 @@
 squareDigits = (num) => {
     return Number(num.toString().split('').map((a) => a*a).join(''))
   }
-console.log(squareDigits(3212))
-console.log(squareDigits(9119));
+// console.log(squareDigits(3212))
+// console.log(squareDigits(9119));
+console.log((3213).toString().split(''));
