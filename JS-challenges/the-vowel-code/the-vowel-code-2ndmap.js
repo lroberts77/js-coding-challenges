278dbe63d41af57702b3dc2297c3ce663b7abf81
Liam R <liamroberts77@googlemail.com>
2021-08-22 23:37:44 +0100

    completed new approach

--- JS-challenges/the-vowel-code/the-vowel-code-2ndmap.js
+++ JS-challenges/the-vowel-code/the-vowel-code-2ndmap.js
@@ -1,4 +1,19 @@
+var vowels = {
+    a:1,
+    e:2,
+    i:3,
+    o:4,
+    u:5
+  };
 
+encode = (string) => {
+    return string.split('').map(e => vowels[e] ? vowels[e] : e).join('');
+  }
+  
+decode = (string) => {
+    return string.split('').map(e => parseInt(e)<6 ? Object.keys(vowels)[e-1] : e).join('');
+  }
+  
 
 console.log(encode('hello'));
 console.log(decode('h2ll4'));
