06388e4e0b2551ae3f12125c8c30b48d29ffac2d
Liam R <liamroberts77@googlemail.com>
2021-08-24 23:03:43 +0100

    complete

--- JS-challenges/loop-array/loop-array.js
+++ JS-challenges/loop-array/loop-array.js
@@ -1,6 +1,6 @@
 loopArr = (arr, direction, steps) => {
     direction == 'left' ? arr.push.apply(arr, arr.splice(0, steps)) :
-    arr.push.apply( arr, arr.splice(0, arr.length - steps ) )
+    arr.push.apply(arr, arr.splice(0, arr.length - steps))
     return arr
   }
 console.log(loopArr([1, 5, 87, 45, 8, 8], 'left', 2));
