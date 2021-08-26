77c98482f4613993a2e0bd8152aa6a227c750c80
Liam R <liamroberts77@googlemail.com>
2021-08-26 22:56:26 +0100

    completed new approach

---
+++ JS-challenges/find-the-stray-number/find-the-stray-number-find.js
@@ -0,0 +1,9 @@
+stray = (numbers) => {
+    return numbers.find((item, i, arr) => {
+        return item !== arr[i+1] && item !== arr[i-1]
+    })
+}
+
+console.log(stray([2, 1, 1]));
+console.log(stray([0, 0, 0, 7, 0, 0, 0]));
+console.log(stray([1,1,2,2,3,4,4,5,5]));
