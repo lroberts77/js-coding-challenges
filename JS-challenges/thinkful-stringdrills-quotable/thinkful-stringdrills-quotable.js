ed9c395e1f3be8b1eb1735e44d9c837e64ca1b24
Liam R <liamroberts77@googlemail.com>
2021-08-26 23:43:33 +0100

    added and passed  tests

--- JS-challenges/thinkful-stringdrills-quotable/thinkful-stringdrills-quotable.js
+++ JS-challenges/thinkful-stringdrills-quotable/thinkful-stringdrills-quotable.js
@@ -1,3 +1,6 @@
 quotable = (name, quote) => {
     return `${name} said: "${quote}"`
 }
+console.log(quotable('Grae', 'Practice makes perfect'));
+console.log(quotable('Alex', 'Ruby is great fun'));
+console.log(quotable('Bethany', 'Yes, way more fun than R'));
