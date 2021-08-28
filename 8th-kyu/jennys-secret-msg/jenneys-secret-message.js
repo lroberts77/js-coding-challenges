48ad15887ef51fab3494f98905dfc1300294de39
Liam R <liamroberts77@googlemail.com>
2021-08-28 23:18:26 +0100

    completed challenge

---
+++ 8th-kyu/jennys-secret-msg/jenneys-secret-message.js
@@ -0,0 +1,7 @@
+greet = (name) => {
+    return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
+
+console.log(greet("Jim"));
+console.log(greet("Jane"))
+console.log(greet("Johnny"));;
