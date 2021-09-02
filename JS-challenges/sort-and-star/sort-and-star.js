7f37f68c7dd3b18495c18e969a23ad4669d560b1
Liam R <liamroberts77@googlemail.com>
2021-09-02 18:31:12 +0100

    added tests

--- JS-challenges/sort-and-star/sort-and-star.js
+++ JS-challenges/sort-and-star/sort-and-star.js
@@ -2,3 +2,6 @@ twoSort = (s) => {
     return s.sort()[0].split('').map((a,b) => b == s[0].length-1 ? a : `${a}***` ).join('')
 }
   
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]));
+console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]));
+console.log(twoSort(["Lets", "all", "go", "on", "holiday", "somewhere", "very", "cold"]));
