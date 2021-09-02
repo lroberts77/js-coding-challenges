c6026cbab011b6c2b7a121d6bfb36b6b01168266
Liam R <liamroberts77@googlemail.com>
2021-09-02 21:35:49 +0100

    completed challenge

--- JS-challenges/well-of-ideas/well-of-ides-refactor.js
+++ JS-challenges/well-of-ideas/well-of-ides-refactor.js
@@ -1,3 +1,7 @@
 well = (x) => {
-    x.includes('good') ? x.filter(s => s == 'good').length < 3 ? 'Publish!' : 'I smell a series!' : 'Fail!';
+    return x.includes('good') ? x.filter(s => s == 'good').length < 3 ? 'Publish!' : 'I smell a series!' : 'Fail!';
 }
+
+console.log(well(['bad', 'bad', 'bad']));
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad']));
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']));
