3c65d3b4009434d7d23ced396b28bca109857128
Liam R <liamroberts77@googlemail.com>
2021-10-14 13:48:03 +0100

    added new tests

--- 8th-kyu/htmlspecialchars/htmlspecialchars-ifelse.js
+++ 8th-kyu/htmlspecialchars/htmlspecialchars-ifelse.js
@@ -16,3 +16,4 @@ htmlspecialchars = (formData) => {
 
   console.log(htmlspecialchars("<h2>Hello World</h2>"));
   console.log(htmlspecialchars("Hello, how would you & I fare?"));
+  console.log(htmlspecialchars('How was "The Matrix"?  Did you like it?'));
