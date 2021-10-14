53164a35b07667caec6749a2ce344dda17ffee2e
Liam R <liamroberts77@googlemail.com>
2021-10-14 13:37:08 +0100

    completed challenge using replace

---
+++ 8th-kyu/htmlspecialchars/htmlspecialchars-replace.js
@@ -0,0 +1,6 @@
+htmlspecialchars = (formData) => {
+    return formData.replace(/"/g, "&quot;").replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
+}
+
+console.log(htmlspecialchars("<h2>Hello World</h2>"));
+console.log(htmlspecialchars("Hello, how would you & I fare?"));
