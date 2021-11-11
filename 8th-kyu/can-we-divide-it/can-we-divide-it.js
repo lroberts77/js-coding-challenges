e6d2fbc82ed0f32d1e86bd3d9f8865410ca37373
Liam R <liamroberts77@googlemail.com>
2021-11-11 22:14:05 +0000

    completed challenge

---
+++ 8th-kyu/can-we-divide-it/can-we-divide-it.js
@@ -0,0 +1,7 @@
+isDivideBy = (number, a, b) => {
+    return number % a == 0 && number % b == 0 ? true : false
+}
+
+console.log(isDivideBy(-12, 2, -6));
+console.log(isDivideBy(-12, 2, -5));
+console.log(isDivideBy(45, 5, 15));
