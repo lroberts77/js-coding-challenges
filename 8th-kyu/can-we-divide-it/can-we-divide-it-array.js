0d8eefd6aa3afc37efeffc0b246bac9bbcec5913
Liam R <liamroberts77@googlemail.com>
2021-11-12 13:56:44 +0000

    added refactor to passed challenge

--- 8th-kyu/can-we-divide-it/can-we-divide-it-array.js
+++ 8th-kyu/can-we-divide-it/can-we-divide-it-array.js
@@ -2,8 +2,12 @@
 //     return [a, b].every(i => number % i === 0)
 // }
 
-isDivideBy = (number, ...val) => {
-    return val.every(i => !(number % i))
+// isDivideBy = (number, ...val) => {
+//     return val.every(i => !(number % i))
+// }
+
+isDivideBy = (...number) => {
+    return number.every(i => number[0] % i === 0)
 }
 
 console.log(isDivideBy(-12, 2, -6));
