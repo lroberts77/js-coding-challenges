40f98c828be1e3afc693bed254958b34fd61547a
Liam R <liamroberts77@googlemail.com>
2021-11-15 19:20:50 +0000

    completed challenge

---
+++ 6th-kyu/persistent-bugger/persistent-bugger.js
@@ -0,0 +1,11 @@
+persistence = function (num, counter = 0) {
+    if ([...`${num}`].length === 1)
+        return counter;
+    num = [...`${num}`].reduce((x, y) => x * y);
+    counter++;
+    return persistence(num, counter);
+}
+
+console.log(persistence(39));
+console.log(persistence(4));
+console.log(persistence(999));
