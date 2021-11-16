4dabf5558d49f0b47667469afd7ade621d76d734
Liam R <liamroberts77@googlemail.com>
2021-11-16 19:46:48 +0000

    refactored completed challenge

---
+++ 6th-kyu/persistent-bugger/persistent-bugger-recurs.js
@@ -0,0 +1,9 @@
+persistence = function (num, counter = 0) {
+    return [...`${num}`].length === 1 ? 
+    counter :
+    persistence([...`${num}`].reduce((x, y) => x * y), ++counter);
+}
+
+console.log(persistence(39));
+console.log(persistence(4));
+console.log(persistence(999));
