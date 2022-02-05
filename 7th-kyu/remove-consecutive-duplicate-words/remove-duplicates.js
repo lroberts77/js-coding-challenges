7b3f53c28eecf4fb82dd1d21b3ab827d95fd2d8f
Liam R <liamroberts77@googlemail.com>
2022-02-05 16:23:45 +0000

    completed challenge

---
+++ 7th-kyu/remove-consecutive-duplicate-words/remove-duplicates.js
@@ -0,0 +1,12 @@
+const removeConsecutiveDuplicates = (s) => {
+    s = s.split(' ')
+    let ans = []
+    for(let i=0;i<s.length;i++) {
+      if (s[i] !== s[i+1]) {
+        ans.push(s[i])
+      }
+    }
+    return ans.join(' ')
+}
+
+console.log(removeConsecutiveDuplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'));
