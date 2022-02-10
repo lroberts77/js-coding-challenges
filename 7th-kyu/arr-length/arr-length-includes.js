3dbac3d73269fedf990777671b542363521bb97e
Liam R <liamroberts77@googlemail.com>
2022-02-10 23:38:58 +0000

    completed refactor

--- 7th-kyu/arr-length/arr-length-includes.js
+++ 7th-kyu/arr-length/arr-length-includes.js
@@ -1,5 +1,7 @@
-
-
+const arrLength = (s) => {
+  return s.includes(s.length) ? 
+  Array(s.length).fill(s.length) :'err'
+}
 
 console.log(arrLength([1,2,3,4,5]));
 console.log(arrLength([17,10,4,8]));
