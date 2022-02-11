70ef3d3bae191d36aba8acc8b2155985bac4efe0
Liam R <liamroberts77@googlemail.com>
2022-02-11 17:06:57 +0000

    completed challenge

---
+++ 8th-kyu/remove-first-last-char/remove.js
@@ -0,0 +1,7 @@
+const removeChar = (str) => {
+  return str.substring(1, str.length - 1);
+};
+
+console.log(removeChar("eloquent"));
+console.log(removeChar("country"));
+console.log(removeChar("place"));
