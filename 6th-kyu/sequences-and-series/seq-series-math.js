c387d5f9b36a15cb4686f0f29220f72a6b00c9cf
Liam R <liamroberts77@googlemail.com>
2022-02-16 14:39:30 +0000

    refactored passed challenge

---
+++ 6th-kyu/sequences-and-series/seq-series-math.js
@@ -0,0 +1,9 @@
+const getScore = (n) => {
+  return n * (n + 1) * 25;
+};
+
+console.log(getScore(1));
+console.log(getScore(2));
+console.log(getScore(3));
+console.log(getScore(4));
+console.log(getScore(5));
