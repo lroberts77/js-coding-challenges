5955ca584fefc357e53a49af80c3b220f97a5590
Liam R <liamroberts77@googlemail.com>
2022-02-18 23:34:50 +0000

    added new challenge

--- JS-challenges/find-the-stray-number/find-the-stray-number-loop.js
+++ JS-challenges/find-the-stray-number/find-the-stray-number-loop.js
@@ -10,5 +10,5 @@ stray = (numbers) => {
 
 console.log(stray([2, 1, 1]));
 console.log(stray([0, 0, 0, 7, 0, 0, 0]));
-console.log(stray([1,1,2,2,3,4,4,5,5]));
+console.log(stray([ 3, 10, 3, 3, 3 ]));
   
