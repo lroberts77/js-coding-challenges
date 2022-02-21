5ff410ad3c94af55049954116953cc5d6a91fc7d
Liam R <liamroberts77@googlemail.com>
2022-02-21 19:44:59 +0000

    formatted challenge

--- 7th-kyu/very-even-numbers/very-even.js
+++ 7th-kyu/very-even-numbers/very-even.js
@@ -1,11 +1,14 @@
 const isVeryEvenNumber = (n) => {
-    let ans = [...`${n}`].map(Number).reduce((a,b) => a + b)
-    return [...`${ans}`].length > 1 ? isVeryEvenNumber(ans) :
-    ans % 2 == 0 ? true : false
-  }
+  let ans = [...`${n}`].map(Number).reduce((a, b) => a + b);
+  return [...`${ans}`].length > 1
+    ? isVeryEvenNumber(ans)
+    : ans % 2 == 0
+    ? true
+    : false;
+};
 
-  console.log(isVeryEvenNumber(0));
-  console.log(isVeryEvenNumber(4));
-  console.log(isVeryEvenNumber(12));
-  console.log(isVeryEvenNumber(222));
-  console.log(isVeryEvenNumber(1234));
+console.log(isVeryEvenNumber(0));
+console.log(isVeryEvenNumber(4));
+console.log(isVeryEvenNumber(12));
+console.log(isVeryEvenNumber(222));
+console.log(isVeryEvenNumber(1234));
