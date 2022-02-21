aa089984b40b9a6cb62c662bf07978f942cc7860
Liam R <liamroberts77@googlemail.com>
2022-02-21 19:42:57 +0000

    completed challenge

---
+++ 7th-kyu/very-even-numbers/very-even.js
@@ -0,0 +1,11 @@
+const isVeryEvenNumber = (n) => {
+    let ans = [...`${n}`].map(Number).reduce((a,b) => a + b)
+    return [...`${ans}`].length > 1 ? isVeryEvenNumber(ans) :
+    ans % 2 == 0 ? true : false
+  }
+
+  console.log(isVeryEvenNumber(0));
+  console.log(isVeryEvenNumber(4));
+  console.log(isVeryEvenNumber(12));
+  console.log(isVeryEvenNumber(222));
+  console.log(isVeryEvenNumber(1234));
