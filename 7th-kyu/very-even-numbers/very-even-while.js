9cff69ba7355a1ccfd7e91de309c2825d196e88f
Liam R <liamroberts77@googlemail.com>
2022-02-21 19:50:12 +0000

    completed challenge using for loop

--- 7th-kyu/very-even-numbers/very-even-while.js
+++ 7th-kyu/very-even-numbers/very-even-while.js
@@ -1,7 +1,8 @@
 const isVeryEvenNumber = (n) => {
-  let ans = [...`${n}`].map(Number).reduce((a, b) => a + b);
-  return [...`${ans}`].length > 1 ? isVeryEvenNumber(ans) :
-    ans % 2 == 0 ? true : false;
+    while (String(n).length > 1) {
+        n = [...`${n}`].map(Number).reduce((a, b) => a + b)
+    }
+    return n % 2 == 0 ? true : false
 };
 
 console.log(isVeryEvenNumber(0));
