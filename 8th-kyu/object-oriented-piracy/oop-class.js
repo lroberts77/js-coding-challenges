adc33b19ea45229a7cd0f46233a2a744bcb5e724
Liam R <liamroberts77@googlemail.com>
2022-02-25 11:19:06 +0000

    completed challenge

---
+++ 8th-kyu/object-oriented-piracy/oop-class.js
@@ -0,0 +1,20 @@
+class Ship {
+  constructor(draft, crew) {
+    this.draft = draft;
+    this.crew = crew;
+  }
+
+  isWorthIt() {
+    return this.draft - (this.crew * 1.5) > 20;
+  }
+}
+
+let emptyShip = new Ship(0,0);
+let aBoat = new Ship(15,20);
+let biggerBoat = new Ship(35,20);
+let aWorthyShip = new Ship(100,20);
+
+console.log(emptyShip.isWorthIt());
+console.log(aBoat.isWorthIt());
+console.log(biggerBoat.isWorthIt());
+console.log(aWorthyShip.isWorthIt());
