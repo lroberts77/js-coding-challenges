d373d272465e7cc9f7a29cbadd4168e7a150df9c
Liam R <liamroberts77@googlemail.com>
2022-02-25 11:14:55 +0000

    completed challenge

---
+++ 8th-kyu/object-oriented-piracy/oop-prototype.js
@@ -0,0 +1,18 @@
+function Ship(draft, crew) {
+  this.draft = draft;
+  this.crew = crew;
+}
+
+Ship.prototype.isWorthIt = function () {
+  return this.draft - this.crew * 1.5 > 20;
+};
+
+let emptyShip = new Ship(0,0);
+let aBoat = new Ship(15,20);
+let biggerBoat = new Ship(35,20);
+let aWorthyShip = new Ship(100,20);
+
+console.log(emptyShip.isWorthIt());
+console.log(aBoat.isWorthIt());
+console.log(biggerBoat.isWorthIt());
+console.log(aWorthyShip.isWorthIt());
