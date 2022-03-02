0af7a26acb4be3af15b8c2dd33a81c63fe9691f5
Liam R <liamroberts77@googlemail.com>
2022-03-02 15:54:14 +0000

    completed challenge

---
+++ 7th-kyu/objects-simple-math-methods/object-math.js
@@ -0,0 +1,21 @@
+Number.prototype.add = function(num) { return this + num };
+
+Number.prototype.sub = function(num) { return this - num };
+
+Number.prototype.mul = function(num) { return this * num };
+
+Number.prototype.div = function(num) { return this / num };
+
+Number.prototype.pow = function(num) { return this ** num };
+
+Number.prototype.sqr = function(num) { return Math.sqrt(this) };
+
+let n = 25;
+
+console.log(n.add(4));
+console.log(n.sub(3));
+console.log(n.mul(2));
+console.log(n.div(5));
+console.log(n.pow(2));
+console.log(n.sqr());
+console.log(n.add(5).div(2).mul(3).sub(35).pow(2).sqr());
