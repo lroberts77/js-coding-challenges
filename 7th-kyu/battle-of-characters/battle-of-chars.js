6fcb9e1cb1f9d4def0094e84f18c272057bf61cc
Liam R <liamroberts77@googlemail.com>
2022-03-04 11:46:56 +0000

    reformat

--- 7th-kyu/battle-of-characters/battle-of-chars.js
+++ 7th-kyu/battle-of-characters/battle-of-chars.js
@@ -2,8 +2,7 @@ const battle = (x, y) => {
     let first = x.split('').map(a => a.charCodeAt(0)-64).reduce((a,b) => a+b)
     let second = y.split('').map(a => a.charCodeAt(0)-64).reduce((a,b) => a+b)
     return first == second ? 'Tie!' :
-    first > second ? x :
-    y
+    first > second ? x : y
 }
 
 console.log(battle("AAA", "Z"));
