afb819a9885719c7ed050bfda7b86d4cc6af5eb0
Liam R <liamroberts77@googlemail.com>
2022-03-04 11:46:40 +0000

    reformat

--- 7th-kyu/battle-of-characters-medium/battle-of-charsm.js
+++ 7th-kyu/battle-of-characters-medium/battle-of-charsm.js
@@ -2,8 +2,7 @@ const battle = (x, y) => {
     let first = x.split('').map(a => /^[A-Z]*$/.test(a) ? a.charCodeAt(0)-64 : (a.charCodeAt(0)-96)/2).reduce((a,b) => a+b)
     let second = y.split('').map(a => /^[A-Z]*$/.test(a) ? a.charCodeAt(0)-64 : (a.charCodeAt(0)-96)/2).reduce((a,b) => a+b)
     return first == second ? 'Tie!' :
-    first > second ? x :
-    y
+    first > second ? x : y
 }
 
 console.log(battle("One", "Two"));
