ea6d91be052a1836519ad2c4ab0ba518052bd888
Liam R <liamroberts77@googlemail.com>
2022-03-04 11:19:24 +0000

    completed challenge

---
+++ 7th-kyu/battle-of-characters/battle-of-chars.js
@@ -0,0 +1,12 @@
+const battle = (x, y) => {
+    let first = x.split('').map(a => a.charCodeAt(0)-64).reduce((a,b) => a+b)
+    let second = y.split('').map(a => a.charCodeAt(0)-64).reduce((a,b) => a+b)
+    return first == second ? 'Tie!' :
+    first > second ? x :
+    y
+}
+
+console.log(battle("AAA", "Z"));
+console.log(battle("ONE", "TWO"));
+console.log(battle("ONE", "NEO"));
+console.log(battle("FOUR", "FIVE"));
