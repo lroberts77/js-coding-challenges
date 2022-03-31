4e08730a0eceb8eabdb98e36ee4cd15ac2dfdacf
Liam R <liamroberts77@googlemail.com>
2022-03-31 17:13:44 +0100

    refactored challenge

---
+++ 7th-kyu/time-converter-hh-mm-ss-ms/time-convert-refactor.js
@@ -0,0 +1,11 @@
+const convert = (time) => {
+    console.log(time.toString());
+    console.log(time.toJSON());
+  return time.toISOString().slice(11, 23).replace(`.`, `,`);
+
+};
+
+console.log(convert(new Date(2016, 2, 8, 16, 42, 59)));
+console.log(convert(new Date(1951, 10, 31, 2, 2, 24, 399)));
+console.log(convert(new Date(1523, 5, 29, 23, 2, 2, 9)));
+console.log(convert(new Date(1978, 3, 18, 12, 0, 0, 0)));
