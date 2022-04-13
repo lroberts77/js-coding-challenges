6410769e973f866d29357adfd3e6485353b1f31f
Liam R <liamroberts77@googlemail.com>
2022-04-13 22:48:44 +0100

    completed challenge

---
+++ 8th-kyu/check-same-case/check-same-case.js
@@ -0,0 +1,11 @@
+const sameCase = (a, b)=> {
+  return a.match(/[A-Z]/) && b.match(/[A-Z]/) 
+    || a.match(/[a-z]/) && b.match(/[a-z]/) ? 1 :
+    !a.match(/[A-Z]/i) || !b.match(/[A-Z]/i) ? -1 :
+    0
+}
+
+console.log(sameCase('C', 'B', 1));
+console.log(sameCase('A', 's', 0));
+console.log(sameCase('A', '?', -1));
+console.log(sameCase('d', 'd', 1));
