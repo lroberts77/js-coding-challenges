d260dc4a5240c4677b41b2632de668e6f2399588
Liam R <liamroberts77@googlemail.com>
2022-04-27 22:18:57 +0100

    added new file to refactor completed challenge

---
+++ 7th-kyu/moment-of-time-in-space/moment-refactor.js
@@ -0,0 +1,5 @@
+console.log(momentOfTimeInSpace("12:30 am"),[false, false, true]);
+console.log(momentOfTimeInSpace("12:02 pm"),[false, true, false]);
+console.log(momentOfTimeInSpace("01:00 pm"),[true, false, false]);
+console.log(momentOfTimeInSpace("11:12 am"),[false, false, true]);
+console.log(momentOfTimeInSpace("05:20 pm"),[false, false, true]);
