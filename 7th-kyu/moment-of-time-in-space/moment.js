ef0d567f41c6b65c90f5d6b43ab43a85bcd9ef07
Liam R <liamroberts77@googlemail.com>
2022-04-27 22:18:03 +0100

    completed challenge

--- 7th-kyu/moment-of-time-in-space/moment.js
+++ 7th-kyu/moment-of-time-in-space/moment.js
@@ -1,3 +1,20 @@
 const momentOfTimeInSpace = (moment) => {
-
+  let arr1 = moment.slice(0,5).replace(/[^\d]/g,'')
+  let arr = arr1.split('')
+  let time = 0
+  let space = 4
+  for(let i=0;i<arr.length;i++) {
+    if (arr[i] === '0') {
+      space++
+    } else {
+      time += Number(arr[i])
+    }    
+  }
+   return [time<space,time==space,time>space]
 }
+
+console.log(momentOfTimeInSpace("12:30 am"),[false, false, true]);
+console.log(momentOfTimeInSpace("12:02 pm"),[false, true, false]);
+console.log(momentOfTimeInSpace("01:00 pm"),[true, false, false]);
+console.log(momentOfTimeInSpace("11:12 am"),[false, false, true]);
+console.log(momentOfTimeInSpace("05:20 pm"),[false, false, true]);
