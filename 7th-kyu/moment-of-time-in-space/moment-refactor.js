d2d6adfe4436d86cec6b8b2af648fa1eae7596d4
Liam R <liamroberts77@googlemail.com>
2022-04-28 15:24:48 +0100

    fixed typo

--- 7th-kyu/moment-of-time-in-space/moment-refactor.js
+++ 7th-kyu/moment-of-time-in-space/moment-refactor.js
@@ -1,3 +1,9 @@
+const momentOfTimeInSpace = (moment) => {
+  let time = moment.split('').filter((el) => parseInt(el)).reduce((a,b) => (+a) + (+b))
+  let space = moment.split('')
+   return [time<space,time==space,time>space]
+}
+
 console.log(momentOfTimeInSpace("12:30 am"),[false, false, true]);
 console.log(momentOfTimeInSpace("12:02 pm"),[false, true, false]);
 console.log(momentOfTimeInSpace("01:00 pm"),[true, false, false]);
