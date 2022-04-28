eaacebf7eb29a6ebe162d6fb00d50f113f4eb6f9
Liam R <liamroberts77@googlemail.com>
2022-04-28 17:03:10 +0100

    refactored completed challenge

---
+++ 7th-kyu/cat-years-dog-years/cat-dog-refactor.js
@@ -0,0 +1,14 @@
+const humanYearsCatYearsDogYears = (humanYears) => {
+  let catYears = 0,
+    dogYears = 0;
+
+  return humanYears < 2
+    ? [humanYears, humanYears * 15, humanYears * 15]
+    : humanYears == 2
+    ? [2, 24, 24]
+    : [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
+};
+
+console.log(humanYearsCatYearsDogYears(1), [1, 15, 15]);
+console.log(humanYearsCatYearsDogYears(2), [2, 24, 24]);
+console.log(humanYearsCatYearsDogYears(10), [10, 56, 64]);
