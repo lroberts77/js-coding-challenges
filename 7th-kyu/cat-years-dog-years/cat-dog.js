f4b7d24b44300d9ca880737f33eaac7fe1a916c2
Liam R <liamroberts77@googlemail.com>
2022-04-28 16:44:10 +0100

    added function with tests

---
+++ 7th-kyu/cat-years-dog-years/cat-dog.js
@@ -0,0 +1,7 @@
+const humanYearsCatYearsDogYears = (humanYears) => {
+
+}
+
+console.log(humanYearsCatYearsDogYears(1), [1,15,15]);
+console.log(humanYearsCatYearsDogYears(2), [2,24,24]);
+console.log(humanYearsCatYearsDogYears(10), [10,56,64]);
