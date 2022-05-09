0aa0fd4acf087b95a7d25844210a5ed4297a85ea
Liam R <liamroberts77@googlemail.com>
2022-05-09 17:19:13 +0100

    completed challenge

---
+++ oop/super-class-extensions/super-class.js
@@ -0,0 +1,19 @@
+class Animal { 
+  constructor(name) {
+    this.name = name;
+  }
+  
+  speak() {
+    return this.name + ' makes a noise, ';
+  }
+}
+
+class Cat extends Animal {
+  get speak() {
+    return `${super.speak()} ${this.name} goes meow.`
+  }
+}
+
+let cat = new Cat('Mr Whiskers');
+
+console.log(cat.speak, 'Mr Whiskers makes a noise, Mr Whiskers goes meow.');
