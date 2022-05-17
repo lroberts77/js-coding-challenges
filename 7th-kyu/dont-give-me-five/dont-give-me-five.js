9580fd80e40aaec3519c15653ba071fbae629c80
Liam R <liamroberts77@googlemail.com>
2022-05-18 00:12:26 +0100

    completed challenge

--- 7th-kyu/dont-give-me-five/dont-give-me-five.js
+++ 7th-kyu/dont-give-me-five/dont-give-me-five.js
@@ -1,3 +1,14 @@
 const dontGiveMeFive = (start, end) => {
+  let arr = [];
+  for (let i = start; i <= end; i++) {
+    if (!i.toString().includes("5")) {
+      arr.push(i);
+    }
+  }
+  return arr.length;
+};
 
-}
+console.log(dontGiveMeFive(1,9), 8)
+console.log(dontGiveMeFive(4,17), 12)
+console.log(dontGiveMeFive(1,90), 72)
+console.log(dontGiveMeFive(-4,17), 20)
