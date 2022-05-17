72567768efb4c432e3ca00df305451506e9fe129
Liam R <liamroberts77@googlemail.com>
2022-05-18 00:52:11 +0100

    refactored completed challenge

--- 7th-kyu/dont-give-me-five/five-refactor.js
+++ 7th-kyu/dont-give-me-five/five-refactor.js
@@ -1,6 +1,10 @@
+const dontGiveMeFive = (start, end) => {
+  return [...Array(end - start + 1).keys()]
+    .map((a) => a + start)
+    .filter((el) => !/5/.test(el)).length;
+};
 
-
-console.log(dontGiveMeFive(1,9), 8)
-console.log(dontGiveMeFive(4,17), 12)
-console.log(dontGiveMeFive(1,90), 72)
-console.log(dontGiveMeFive(-4,17), 20)
+console.log(dontGiveMeFive(1, 9), 8);
+console.log(dontGiveMeFive(4, 17), 12);
+console.log(dontGiveMeFive(1, 90), 72);
+console.log(dontGiveMeFive(-4, 17), 20);
