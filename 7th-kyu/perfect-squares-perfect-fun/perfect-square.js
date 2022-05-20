d8286ad6d3396717186fe5c9060de6d8bb4c16cc
Liam R <liamroberts77@googlemail.com>
2022-05-20 14:32:52 +0100

    completed challenge

---
+++ 7th-kyu/perfect-squares-perfect-fun/perfect-square.js
@@ -0,0 +1,20 @@
+const squareIt = (int) => {
+  let ans = ''
+  let array = [...`${int}`]
+  let square = Math.sqrt([...`${int}`].length)
+  if(!Number.isInteger(square)) {
+    return 'Not a perfect square!'
+  }
+  for(let i=0;i<array.length;i++) {
+    if(i>1 && i%square === 0) {
+      ans += '\n'
+    }
+    ans += array[i]
+  }
+  return ans
+}
+
+console.log(squareIt(1), "1");
+console.log(squareIt(222), "Not a perfect square!");
+console.log(squareIt(234562342342), "Not a perfect square!");
+console.log(squareIt(112141568), "112\n141\n568");
