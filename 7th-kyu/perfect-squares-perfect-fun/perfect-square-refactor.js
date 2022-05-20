589988c571c8a9f1b82aa42ee5e7bea8fa79bfd3
Liam R <liamroberts77@googlemail.com>
2022-05-20 14:33:53 +0100

    added new file to refactor passed challenge

---
+++ 7th-kyu/perfect-squares-perfect-fun/perfect-square-refactor.js
@@ -0,0 +1,8 @@
+const squareIt = (int) => {
+
+}
+
+console.log(squareIt(1), "1");
+console.log(squareIt(222), "Not a perfect square!");
+console.log(squareIt(234562342342), "Not a perfect square!");
+console.log(squareIt(112141568), "112\n141\n568");
