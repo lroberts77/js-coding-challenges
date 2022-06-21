87d47c73dbfb2433652c5c6268d776d2494b546e
Liam R <liamroberts77@googlemail.com>
2022-06-21 18:32:54 +0100

    refactored completed challenge using forEach

---
+++ oop/coding-meetup-2-higher-order-functions/meetup-foreach.js
@@ -0,0 +1,14 @@
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+const greetDevelopers = (list) => {
+  list.forEach((developer) => {
+    developer.greeting = `Hi ${developer.firstName}, what do you like the most about ${developer.language}?`;
+  });
+  return list;
+}
+
+console.log(greetDevelopers(list1));
