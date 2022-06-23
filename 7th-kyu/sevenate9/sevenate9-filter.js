524b6a456570a5526836036dbc5e7543987493fd
Liam R <liamroberts77@googlemail.com>
2022-06-23 16:40:26 +0100

    refactored completed challenge

--- 7th-kyu/sevenate9/sevenate9-filter.js
+++ 7th-kyu/sevenate9/sevenate9-filter.js
@@ -1,5 +1,15 @@
-console.log(sevenAte9('797'), '77');
-console.log(sevenAte9('7979797'), '7777');
-console.log(sevenAte9('165561786121789797'), '16556178612178977');
-console.log(sevenAte9('979') , '979');
-console.log(sevenAte9('9797979'), '97779');
+const sevenAte9 = (str) => {
+  return [...str]
+    .filter(
+      (el, idx, arr) =>
+        el !== "9" ||
+        !(el === "9" && arr[idx - 1] === "7" && arr[idx + 1] === "7")
+    )
+    .join("");
+};
+
+console.log(sevenAte9("797"), "77");
+console.log(sevenAte9("7979797"), "7777");
+console.log(sevenAte9("165561786121789797"), "16556178612178977");
+console.log(sevenAte9("979"), "979");
+console.log(sevenAte9("9797979"), "97779");
