f640abfcb78e59d34cfbecc0c5925ed4771646bc
Liam R <liamroberts77@googlemail.com>
2022-06-23 16:35:17 +0100

    try and solve challenge using filter

---
+++ 7th-kyu/sevenate9/sevenate9-filter.js
@@ -0,0 +1,5 @@
+console.log(sevenAte9('797'), '77');
+console.log(sevenAte9('7979797'), '7777');
+console.log(sevenAte9('165561786121789797'), '16556178612178977');
+console.log(sevenAte9('979') , '979');
+console.log(sevenAte9('9797979'), '97779');
