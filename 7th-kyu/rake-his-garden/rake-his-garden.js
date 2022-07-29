a8dbfc461da8b7d098d78ae482cdb0f69831c241
Liam R <liamroberts77@googlemail.com>
2022-07-29 15:10:54 +0100

    completed with different or syntax

--- 7th-kyu/rake-his-garden/rake-his-garden.js
+++ 7th-kyu/rake-his-garden/rake-his-garden.js
@@ -1,5 +1,9 @@
+// const rakeGarden = (garden) => {
+//   return garden.split(' ').map(a => a !== 'rock' ? 'gravel' : a).join(' ')
+// }
+
 const rakeGarden = (garden) => {
-  return garden.split(' ').map(a => a !== 'rock' ? 'gravel' : a).join(' ')
+  return garden.split(' ').map(a => a !== ('rock' || 'gravel') ? 'gravel' : a).join(' ')
 }
 
 console.log(rakeGarden('slug spider rock gravel gravel gravel gravel gravel gravel gravel'));
