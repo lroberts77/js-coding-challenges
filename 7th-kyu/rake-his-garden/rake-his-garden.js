872f2650e1e7601329053868d5b2f8f8beec0f20
Liam R <liamroberts77@googlemail.com>
2022-07-29 15:08:31 +0100

    completed challenge

---
+++ 7th-kyu/rake-his-garden/rake-his-garden.js
@@ -0,0 +1,7 @@
+const rakeGarden = (garden) => {
+  return garden.split(' ').map(a => a !== 'rock' ? 'gravel' : a).join(' ')
+}
+
+console.log(rakeGarden('slug spider rock gravel gravel gravel gravel gravel gravel gravel'));
+console.log(rakeGarden('gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'));
+console.log(rakeGarden('gravel rock gravel snail weed'));
