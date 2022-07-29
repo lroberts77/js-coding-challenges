87ecaf6936f672f0089bae4a7e2dcc9f97cbb5ba
Liam R <liamroberts77@googlemail.com>
2022-07-29 15:16:34 +0100

    completed challenge using replace

--- 7th-kyu/rake-his-garden/rake-his-garden-replace.js
+++ 7th-kyu/rake-his-garden/rake-his-garden-replace.js
@@ -1,3 +1,6 @@
+const rakeGarden = (garden) => {
+   return garden.replace(/(?!rock\b)\b\w+/g,'gravel');
+}
 
 console.log(rakeGarden('slug spider rock gravel gravel gravel gravel gravel gravel gravel'));
 console.log(rakeGarden('gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'));
