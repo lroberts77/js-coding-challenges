66fcce5000310e9b173363f418300bb7b0efd9ee
Liam R <liamroberts77@googlemail.com>
2022-07-29 15:13:44 +0100

    task to complete challenge using replace

---
+++ 7th-kyu/rake-his-garden/rake-his-garden-replace.js
@@ -0,0 +1,4 @@
+
+console.log(rakeGarden('slug spider rock gravel gravel gravel gravel gravel gravel gravel'));
+console.log(rakeGarden('gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'));
+console.log(rakeGarden('gravel rock gravel snail weed'));
